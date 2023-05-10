a9a2097685a16a9c425c2c09618365e896e73b10
XuShuJia <13428342592@163.com>
2023-05-10 19:06:01 +0800

    update

--- src/components/ModuleLoading/ModuleLoading.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import styles from "./style.module.less";
-</script>
-
-<template>
-  <div data-tt="true" :class="styles['module-loading']">
-    LOADING...
-  </div>
-</template>
--- src/mock/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as createUserInfo } from "./createUserInfo";
-export { default as createUserMenus } from "./createUserMenus";
-export { default as createUserModules } from "./createUserModules";
-export { default as createLoginResData } from "./createLoginResData";
